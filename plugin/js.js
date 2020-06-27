dc0d6deaee232b178dad1ca9a276d75a1a579dda
jimmyyen67 <ichthyoid95@gmail.com>
2020-06-27 23:08:38 +0800

    才完成一點點

---
+++ plugin/js.js
@@ -0,0 +1,7 @@
+function toSection2() {
+  document.getElementById('section2').scrollIntoView();
+  document.getElementById('navbarSupportedContent').style.display = "none";
+  }
+function toTop(){
+  $(window).scrollTop(0); 
+}
