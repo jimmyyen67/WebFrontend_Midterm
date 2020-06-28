799272d11303f605d353c4c4e4a10140ca3f5235
jimmyyen67 <ichthyoid95@gmail.com>
2020-06-28 15:07:53 +0800

    導覽列提整

--- plugin/js.js
+++ plugin/js.js
@@ -1,7 +1,26 @@
 function toSection2() {
-  document.getElementById('section2').scrollIntoView();
-  document.getElementById('navbarSupportedContent').style.display = "none";
-  }
-function toTop(){
-  $(window).scrollTop(0); 
+  document.getElementById("section2").scrollIntoView();
+  document.getElementById("navbarSupportedContent").style.height = "0";
 }
+function toTop() {
+  $(window).scrollTop(0);
+}
+
+var navbar = document.getElementById("navbar");
+var navbarbrand = document.getElementById("navbar-brand");
+window.onscroll = function () {
+  if (
+    document.body.scrollTop >= 70 ||
+    document.documentElement.scrollTop >= 70
+    ) {
+      navbar.classList.add("nav-transparent");
+      navbar.classList.remove("nav-colored");
+      navbarbrand.classList.add("brand-transparent");
+      brandbarbrand.classList.remove("brand-colored");
+  } else {
+    navbar.classList.add("nav-colored");
+    navbar.classList.remove("nav-transparent");
+    navbarbrand.classList.add("brand-colored");
+    navbarbrand.classList.remove("brand-transparent");
+  }
+};
